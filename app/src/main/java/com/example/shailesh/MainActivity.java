5f67a7f2f4f949c059900b37ef89b51034cafd29
shailesh <uic.17bca1297@gmail.com>
2020-01-24 13:53:47 +0530

    activity life cycle

--- app/src/main/java/com/example/shailesh/MainActivity.java
+++ app/src/main/java/com/example/shailesh/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.shailesh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"OnCreate",Toast.LENGTH_LONG).show();
+                                //getApplicationContext()
+    }
+
+    protected void toast(String msg)
+    {
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+    }
+@Override
+    protected void onStart()
+    {
+        super.onStart();
+        toast("OnStart");
+       // Toast.makeText(getApplicationContext(),"OnStart",Toast.LENGTH_SHORT).show();
+    }
+@Override
+    protected  void onPause()
+    {
+        super.onPause();
+        toast("OnPause");
+
+    }
+@Override
+    protected void onResume()
+    {
+        super.onResume();
+        toast("OnResume");
+    }
+@Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        toast("OnDestroy");
+    }
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        toast("OnRestart");
     }
 }
