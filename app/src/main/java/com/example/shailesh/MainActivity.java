32e2555cac7ac72c04597315321a193d23300979
shailesh <uic.17bca1297@gmail.com>
2020-02-28 13:34:07 +0530

    removed register module

--- app/src/main/java/com/example/shailesh/MainActivity.java
+++ app/src/main/java/com/example/shailesh/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.shailesh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.shailesh.calculator.Calculator;
-import com.example.shailesh.registration.Registration;
 
 import com.example.shailesh.simplecalculator.SimpleCalculator;
 
@@ -34,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(calintent);
             }
         });
+
+
+        //Explicit Intent
         butCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,13 +46,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Implicit Intent
         butReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent calintent=new Intent(getBaseContext(), Registration.class);
-                startActivity(calintent);
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com/"));
+                startActivity(intent);
             }
         });
+
+
     }
 
     public void toast(String msg)
